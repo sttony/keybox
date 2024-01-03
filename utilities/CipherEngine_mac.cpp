6ada5a75a5427e7ec1e0f13c819d3a4087f94038
sttony <sttony1984@gmail.com>
2024-01-03 11:38:35 -0800

    macOS SHA256 implementation

--- utilities/CipherEngine_mac.cpp
+++ utilities/CipherEngine_mac.cpp
@@ -3,15 +3,28 @@
 #include <iostream>
 #include <functional>
 #include <utility>
+#include <CommonCrypto/CommonDigest.h>
 
 
 uint32_t CCipherEngine::SHA256(const unsigned char *pInput, size_t cbInput, std::vector<unsigned char> &Output) {
+    CC_SHA256_CTX sha256;
+    CC_SHA256_Init(&sha256);
 
-
+    CC_SHA256_Update(&sha256, pInput, static_cast<CC_LONG>(cbInput));
+    Output.resize(32);
+    CC_SHA256_Final(&Output[0], &sha256);
     return 0;
 }
 
 uint32_t CCipherEngine::SHA256(const unsigned char *pInput, size_t cbInput, unsigned char *pOutput, size_t cbOutput) {
+    if(cbOutput<32){
+        return ERROR_BUFFER_TOO_SMALL;
+    }
+    CC_SHA256_CTX sha256;
+    CC_SHA256_Init(&sha256);
+
+    CC_SHA256_Update(&sha256, pInput, static_cast<CC_LONG>(cbInput));
+    CC_SHA256_Final(pOutput, &sha256);
     return 0;
 }
 
