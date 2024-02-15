7f511f413cc35a88bfec7b9b1422cdf795d7ca1f
sttony <sttony@protonmail.com>
2024-02-14 19:03:38 -0800

    Base64 encode

--- boost_tests/Base64Test.cpp
+++ boost_tests/Base64Test.cpp
@@ -26,5 +26,18 @@ BOOST_AUTO_TEST_SUITE(Base64TestSuit)
         BOOST_CHECK_EQUAL(output2[4], 0x05);
         BOOST_CHECK_EQUAL(output2[5], 0x00);
     }
+    BOOST_AUTO_TEST_CASE(encode)
+    {
+        Base64Coder base64Coder;
+        string output ;
+        base64Coder.Encode((unsigned char*)"hello world!", sizeof("hello world!")-1, output);
+        BOOST_CHECK_EQUAL(output, "aGVsbG8gd29ybGQh");
+
+        vector<unsigned char> input1 = {0x01, 0x02, 0x03, 0x04, 0x05, 0x00};
+        output="";
+        base64Coder.Encode(&input1[0], 6, output);
+        BOOST_CHECK_EQUAL(output, "AQIDBAUA");
+    }
+
 
 BOOST_AUTO_TEST_SUITE_END()
