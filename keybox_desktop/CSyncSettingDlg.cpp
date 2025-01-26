f954bf9ffcced2115b4b1dc40a59a3ae32c8357b
sttony <sttony@protonmail.com>
2025-01-25 18:34:09 -0800

    Add email and url sync boxes

--- keybox_desktop/CSyncSettingDlg.cpp
+++ keybox_desktop/CSyncSettingDlg.cpp
@@ -5,9 +5,38 @@
 #include <QVBoxLayout>
 #include "CSyncSettingDlg.h"
 
+
+
 CSyncSettingDlg::CSyncSettingDlg(QWidget *parent) {
     QVBoxLayout *rootLayout = new QVBoxLayout(this);
 
-//    m_chkboxUpper = new QCheckBox("Upper case A-Z");
-//    rootLayout->addWidget(m_chkboxUpper);
+
+    QHBoxLayout * emailLine = new QHBoxLayout;
+    m_emailBox  = new QLineEdit();
+    emailLine->addWidget(new QLabel("Email"));
+    emailLine->addWidget(m_emailBox);
+    rootLayout->addLayout(emailLine);
+
+
+    QHBoxLayout * syncUrlLine = new QHBoxLayout;
+    m_syncUrlBox= new QLineEdit();
+    syncUrlLine->addWidget(new QLabel("Sync server"));
+    syncUrlLine->addWidget(m_syncUrlBox);
+    rootLayout->addLayout(syncUrlLine);
+
+
+    QHBoxLayout *btnLine2 = new QHBoxLayout();
+    m_saveBtn = new QPushButton("Save");
+    m_cancelBtn = new QPushButton("Cancel");
+    btnLine2->addWidget(m_saveBtn);
+    btnLine2->addWidget(m_cancelBtn);
+    rootLayout->addLayout(btnLine2);
+    setLayout(rootLayout);
+
+    connect(m_saveBtn, &QPushButton::clicked, this, &CSyncSettingDlg::onSave);
+    QObject::connect(m_saveBtn, &QPushButton::clicked, this, &QDialog::reject);
+}
+
+void CSyncSettingDlg::onSave() {
+
 }
--- keybox_desktop/CSyncSettingDlg.h
+++ keybox_desktop/CSyncSettingDlg.h
@@ -5,14 +5,23 @@
 #ifndef KEYBOX_CSYNCSETTINGDLG_H
 #define KEYBOX_CSYNCSETTINGDLG_H
 #include <QDialog>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
 
 class CSyncSettingDlg : public QDialog {
 Q_OBJECT
 public:
     explicit CSyncSettingDlg(QWidget *parent = nullptr);
 
-private:
+private slots:
+    void onSave();
 
+private:
+    QLineEdit * m_emailBox = nullptr;
+    QLineEdit * m_syncUrlBox = nullptr;
+    QPushButton* m_saveBtn = nullptr;
+    QPushButton* m_cancelBtn = nullptr;
 
 };
 
