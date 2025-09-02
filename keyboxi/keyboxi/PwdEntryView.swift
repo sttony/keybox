bd5310b1ba26336d5d199af9a5c7cda0efc632e1
sttony <sttony@protonmail.com>
2025-09-02 15:28:16 -0700

    Add PwdEntry view

---
+++ keyboxi/keyboxi/PwdEntryView.swift
@@ -0,0 +1,22 @@
+
+
+import SwiftUI
+
+struct PwdEntryView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            
+        }
+        .padding()
+        .onAppear(){
+        }
+    }
+}
+
+#Preview {
+    PwdEntryView()
+}
