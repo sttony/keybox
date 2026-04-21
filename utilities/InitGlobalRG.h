ddac3f78ad7a1fefe83da24fc42ea12e8b9c59c8
sttony <sttony@protonmail.com>
2026-04-21 10:35:08 -0700

    New register for iOS.

--- utilities/InitGlobalRG.h
+++ utilities/InitGlobalRG.h
@@ -5,11 +5,18 @@
 #ifndef KEYBOX_INITGLOBALRG_H
 #define KEYBOX_INITGLOBALRG_H
 
+#ifdef __cplusplus
 #include "CRandomGenerator.h"
 
 extern CRandomGenerator g_RG;
 
+extern "C" {
+#endif
+
 void InitGlobalRG();
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif //KEYBOX_INITGLOBALRG_H
--- utilities/InitGlobalRG_mac.cpp
+++ utilities/InitGlobalRG_mac.cpp
@@ -3,6 +3,7 @@
 //
 #include <unistd.h>
 #include <sys/time.h>
+#include <Security/Security.h>
 #include "CipherEngine.h"
 #include "InitGlobalRG.h"
 
@@ -14,9 +15,14 @@ void InitGlobalRG() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     uint32_t final = current_pid + tv.tv_usec;
-    CCipherEngine cipherEngine;
-    vector<unsigned char> buff32;
-    cipherEngine.SHA256(reinterpret_cast<unsigned char *>(&final), sizeof(current_pid), buff32);
+    
+    vector<unsigned char> buff32(32);
+    if (SecRandomCopyBytes(kSecRandomDefault, buff32.size(), buff32.data()) != errSecSuccess) {
+        // Fallback to less secure entropy if SecRandomCopyBytes fails
+        CCipherEngine cipherEngine;
+        cipherEngine.SHA256(reinterpret_cast<unsigned char *>(&final), sizeof(final), buff32);
+    }
+    
     static const std::array<unsigned char, 8> vIV = {0xe8, 0x30, 0x09, 0x4b, 0x97, 0x20, 0x5d, 0x2a};
     g_RG.init(buff32, vIV);
 }
